04bb33565a857f7743644a48147b61f4742de3bf
bodelha <bodelha@gmail.com>
2024-04-06 23:10:32 -0300

    Sem logs de erro

--- api/main.go
+++ api/main.go
@@ -1,25 +1,21 @@
 package main
 
-
-import "src/database"
-
 import (
-    "context"
-    "fmt"
-    "net/http"
+	"api/database"
 
+	"context"
 
-    "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-    ctx := context.Background()
-    err := database.Connect(ctx) // Invoque a função Connect sem passar a URI
-    if err != nil {
-        panic(err)
-    }
+	ctx := context.Background()
+	err := database.Connect(ctx)
+	if err != nil {
+		panic(err)
+	}
 
-    router := gin.Default()
+	router := gin.Default()
 
-    router.Run("localhost:8080")
+	router.Run(:3000)
 }
