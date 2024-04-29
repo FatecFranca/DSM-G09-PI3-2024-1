e773072e5442581754c902180eb6306f68cdf9c7
bodelha <bodelha@gmail.com>
2024-04-28 21:06:49 -0300

    Conexão com o banco

---
+++ api/database/connect.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const MONGO_HOST = process.env.MONGO_HOST;
+const MONGO_PORT = process.env.MONGO_PORT;
+const MONGO_DATABASE = process.env.MONGO_DATABASE;
+
+const mongoURL = `mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DATABASE}`;
+
+const mongooseOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+async function connectToMongoDB() {
+  try {
+    await mongoose.connect(mongoURL, mongooseOptions);
+    console.log('Conexão com o MongoDB estabelecida com sucesso!');
+  } catch (error) {
+    console.error('Erro ao conectar ao MongoDB:', error);
+  }
+}
+
+module.exports = connectToMongoDB;
--- api/index.js
+++ api/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const routes = require('./routes');
+const connectToMongoDB = require('./database/connect');
+
+connectToMongoDB();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
