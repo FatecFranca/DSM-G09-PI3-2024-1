395d9b0fb7e53f6ed8947d9717c0b672d2dc4572
izabelatrc <izabela.coimbra@fatec.sp.gov.br>
2024-06-05 01:56:52 -0300

    Atualização links Footer

--- app/src/components/Footer/Footer.js
+++ app/src/components/Footer/Footer.js
@@ -7,8 +7,8 @@ function Footer() {
     return (
         <footer className="footer">
             <div className="footer-icons">
-                <Link to='https://github.com/fatecfranca/DSM-G09-PI3-2024-1'><i class="bi bi-github"></i></Link>
-                <Link to='https://www.youtube.com/izabelatrc'><i class="bi bi-youtube"></i></Link>
+                <Link to='https://github.com/fatecfranca/DSM-G09-PI3-2024-1' target='_blank'><i class="bi bi-github"></i></Link>
+                <Link to='https://www.youtube.com/izabelatrc'target='_blank'><i class="bi bi-youtube"></i></Link>
             </div>
             <p className="footer-text">© 2024 Wanderlust</p>
             <Link to='/about' className="footer-text">Sobre</Link>
