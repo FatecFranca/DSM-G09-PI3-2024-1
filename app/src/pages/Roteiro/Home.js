75779cc2425d21943b26bda2f9e759ca54af3df0
izabelatrc <izabela.coimbra@fatec.sp.gov.br>
2024-06-10 21:16:14 -0300

    get na api para Home-Roteiro

--- app/src/pages/Roteiro/Home.js
+++ app/src/pages/Roteiro/Home.js
@@ -3,52 +3,42 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-function HomeRoteiros() {   
+function HomeRoteiros() { 
+    const [roteiros, setRoteiros] = useState([]);
+
+    useEffect(() => {
+        const fetchRoteiros = async () => {
+            try {
+                const response = await axios.get('http://localhost:3000/roteiros');
+                const sortedRoteiros = response.data.sort((a, b) => new Date(b.data) - new Date(a.data));
+                setRoteiros(sortedRoteiros);
+            } catch (error) {
+                console.error('Erro ao buscar roteiros:', error);
+            }
+        };
+
+        fetchRoteiros();
+    }, []);
+
     return (
         <div className="container" id= "base">
-            <div className="content" id='card'>
-                <Link to="/roteiro">
-                    <div className='card-roteiro'>
-                        <img src="https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"></img>
-                    </div>
-                </Link>
-                <h2>Cidade</h2>
-            </div>
-            <div className="content" id='card'>
-                <Link to="/roteiro">
-                    <div className='card-roteiro'>
-                        <img src="https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"></img>
-                    </div>
-                </Link>
-                <h2>Cidade</h2>
-            </div>
-            <div className="content" id='card'>
-                <Link to="/roteiro">
-                    <div className='card-roteiro'>
-                        <img src="https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"></img>
-                    </div>
-                </Link>
-                <h2>Cidade</h2>
-            </div>
-            <div className="content" id='card'>
-                <Link to="/roteiro">
-                    <div className='card-roteiro'>
-                        <img src="https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"></img>
-                    </div>
-                </Link>
-                <h2>Cidade</h2>
-            </div>
-            <div className="content" id='card'>
-                <Link to="/roteiro">
-                    <div className='card-roteiro'>
-                        <img src="https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"></img>
-                    </div>
-                </Link>
-                <h2>Cidade</h2>
-            </div>
             <div className="content button" id='card'>
                 <button className="btn create"><Link to="/roteiro"><i class="bi bi-plus"></i></Link></button>
+            </div>
+            <div className="content" id='card'>
+                {roteiros.map((roteiro, index) => (
+                    <div key={index} id = "card">
+                        <Link to="/roteiro">
+                            <div key={index} className='card-roteiro roteiro'>
+                                <img src={roteiro.img} alt={roteiro.cidade}></img>
+                            </div>
+                        </Link>
+                        <h2>{roteiro.cidade}</h2>
+                    </div>
+                ))}
             </div>
         </div>
     );
